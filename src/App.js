d66a7b1f27739c9ebd85e7c65ac9a45e7765d1d5
Rebecca Haggard <beccahagg5@gmail.com>
2020-02-06 18:46:12 -0700

    navbar sidebar layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <Navbar className="navbar" expand="lg">
+              {/*<img*/}
+              {/*    src=""*/}
+              {/*    width="30"*/}
+              {/*    height="30"*/}
+              {/*    className="d-inline-block align-top"*/}
+              {/*    alt="logo"*/}
+              {/*/>*/}
+              <Navbar.Brand href="#home"><p className="brand" >GiftEx</p></Navbar.Brand>
+          </Navbar>
+          <div className="sidebar">
+              <Nav defaultActiveKey="/home" className="flex-column">
+                  <Nav.Link className="nav-link" href="/home">Jones Family</Nav.Link>
+                  <Nav.Link className="nav-link">Wishlists
+                      <Nav.Link href='#' className="nav-link">Sarah Jones</Nav.Link>
+                      <Nav.Link href='#' className="nav-link">Mike Jones</Nav.Link>
+                      <Nav.Link href='#' className="nav-link">Mo Jones</Nav.Link>
+                  </Nav.Link>
+              </Nav>
+          </div>
+      </div>
   );
 }
 
