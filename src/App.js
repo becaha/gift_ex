d7420fc607fc9a3a35c9eb72546152b40846ca3a
Rebecca Haggard <beccahagg5@gmail.com>
2020-02-20 13:14:06 -0700

    log in with dark mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
 import { LogoHeader } from './LogoHeader.js';
 import { Screen } from './Screen.js';
+import Button from "react-bootstrap/Button";
 
 
 function App() {
@@ -28,14 +29,19 @@ export class Main extends React.Component{
       color: '#116661',
       textAlign:'center'
     }
+    this.mode = "Light";
     this.changePage = this.changePage.bind(this);
-    this.state = {page:'home', pageContent:(<div><LogoHeader/>
-        <div><p style={mainheading}>Gift Organized</p><div style = {subheading}><p >Keep All those loose documents together</p><button onClick={this.changePage}>LOG IN
-        </button></div></div></div>),
-      members:['Sarah Jones', 'Mike Jones', 'Mo Jones'],groupName:"Jones Family",
+    this.darkMode = this.darkMode.bind(this);
+      this.state = {page:'home', pageContent:(
+        <div><LogoHeader/>
+        <div><p style={mainheading}>Gift Organized</p><div style = {subheading}><p >Keep All those loose documents together</p>
+            <Button className="btn-login" onClick={this.changePage}>LOG IN</Button>or
+            <Button className="btn-login" onClick={this.darkMode}>LOG IN in Dark Mode</Button>
+        </div></div></div>),
+      members:['Sarah Jones', 'Mike Jones', 'Mo Jones'],
+        groupName:"Jones Family",
       activityLists: {"Sarah Jones":["Tesla Model X","IPhone 11"],"Mike Jones":["Red Scarf","Cuff Links"]}
     };
-
   }
 
   changePage(){
@@ -44,13 +50,20 @@ export class Main extends React.Component{
         <Screen groupName={this.state.groupName} members={this.state.members} giftLists={this.state.activityLists}/>
       </div>)});
   }
+
+  darkMode() {
+      console.log("toggle mode");
+      this.mode = 'Dark';
+      this.changePage();
+  }
+
   render(){
-    let darkmode = true;
-    if(darkmode) {
+      console.log("app render");
+    if(this.mode === 'Dark') {
       require('./DarkApp.css');
     }
     else {
-      require('./App.css');
+      require('./LightMode.css');
     }
     return (
       <div>{this.state.pageContent}</div>
