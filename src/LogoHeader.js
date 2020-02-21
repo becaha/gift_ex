e231b65a9334b6ed25a70d0289ee20b90e299cf3
Rebecca Haggard <beccahagg5@gmail.com>
2020-02-21 14:18:43 -0700

    sticky header

--- src/LogoHeader.js
+++ src/LogoHeader.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import Navbar from "react-bootstrap/Navbar";
-// import MainLogo from "./gift_icon_W.png";
+import MainLogo from "./assets/gift_icon_W.png";
 
 export class LogoHeader extends React.Component {
     render() {
         const imagestyle = {height:"30px",margin:"10px"}
-        // return <nav>{navLinks}</nav>;
         return <Navbar className="navbar" expand="lg">
-            {/*<img*/}
-            {/*    src=""*/}
-            {/*    width="30"*/}
-            {/*    height="30"*/}
-            {/*    className="d-inline-block align-top"*/}
-            {/*    alt="logo"*/}
-            {/*/>*/}
-            {/*<img src={MainLogo} style={imagestyle}/>*/}
-            <Navbar.Brand href="#home"><p className="brand" >GiftEx</p></Navbar.Brand>
+            <img src={MainLogo} style={imagestyle}/>
+            <Navbar.Brand><p className="brand" >GiftEx</p></Navbar.Brand>
         </Navbar>;
     }
 }
